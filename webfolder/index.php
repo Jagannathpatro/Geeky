d1b55cee7f4c7a44717e7128ad60b7aac96f64de
Jagannathpatro <67574727+Jagannathpatro@users.noreply.github.com>
2020-09-01 13:19:00 +0530

    Update index.php

--- webfolder/index.php
+++ webfolder/index.php
@@ -18,8 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 // output data of each row
 while($row = $result->fetch_assoc()) {
-echo "name: " . $row["firstname"]. " - rollnumber: " . $row["rollnumber"]."<br>";
-echo "Hi this is geeky";
+echo "name: " . $row["firstname"]. " ---- rollnumber: " . $row["rollnumber"]."<br>";
 }
 } else {
 echo "0 results";
